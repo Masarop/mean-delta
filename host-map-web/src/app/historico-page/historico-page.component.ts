60bd3c4e881618710ed1f7a934ec81b805f3ef09
Masarop <lucasarrudadeoliveira@gmail.com>
2017-05-30 12:41:39 -0300

    função consultar

--- host-map-web/src/app/historico-page/historico-page.component.ts
+++ host-map-web/src/app/historico-page/historico-page.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Localizacao} from "../model/localizacao.model";
 import {HttpClientService} from "../http-client.service";
-import {forEach} from "@angular/router/src/utils/collection";
-import {ActivatedRoute, Router} from "@angular/router";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'fd-historico-page',
@@ -15,36 +14,17 @@ export class HistoricoPageComponent implements OnInit {
 
 
 
-    constructor(private _router: Router, private httpClient: HttpClientService) {
-    }
-
-
-
-    consultar(ip):void{
-
-
-
-        _router.navigateByUrl(["/map",ip]);
-
+    constructor(private httpClient: HttpClientService, private _router:Router) {
     }
 
     ngOnInit() {
-
-        this._router.params.subscribe((params) => {
-            let ip = params['ip'];
-
-            if (ip) {
-                this..findById(ip).subscribe(
-                    (note) => this.note = note,
-                    (err) => this.msgUtil.addAlertError(err.message)
-                );
-            }
-        });
-
             this.httpClient
             .get('http://localhost:3000/api/localizacao')
             .subscribe((docs) => {
                 this.historico = docs
             });
+    }
+    consultar(query:String){
+        this._router.navigate(['/map', query]);
     }
 }
