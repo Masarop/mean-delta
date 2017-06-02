f0a40d4000dcc6bf18fc6661dc25fff6ce9b8083
Masarop <lucasarrudadeoliveira@gmail.com>
2017-06-02 08:59:15 -0300

    Excluir

--- host-map-web/src/app/historico-page/historico-page.component.ts
+++ host-map-web/src/app/historico-page/historico-page.component.ts
@@ -12,8 +12,6 @@ export class HistoricoPageComponent implements OnInit {
 
     historico: Localizacao[];
 
-
-
     constructor(private httpClient: HttpClientService, private _router:Router) {
     }
 
@@ -24,7 +22,38 @@ export class HistoricoPageComponent implements OnInit {
                 this.historico = docs
             });
     }
+
     consultar(query:String){
         this._router.navigate(['/map', query]);
+    }
+
+    excluir(localizacao): void{
+
+        this.httpClient.delete('http://localhost:3000/api/localizacao?id='+localizacao._id)
+            .subscribe(
+                (ret)=>{
+                    console.log(ret);
+                    if(ret.situacao == 'removido'){
+                        let index = -1;
+
+                        for( let i = 0; i < this.historico.length; i++ ) {
+                            if( this.historico[i] === localizacao ) {
+                                index = i;
+                                break;
+                            }
+                        }
+                        if( index === -1 ) {
+                            alert( "Item não encontrado na lista para remoção" ); return;
+                        }
+
+                        this.historico.splice( index, 1 );
+                    }else{
+                        alert( "Erro ao tentar remover. Nenhum item foi removido" ); return;
+                    }
+
+                },
+                (err)=>{
+                    alert( "Ocorreu um erro ao tentar remover o Item. Nenhum item foi removido" ); return;
+                });
     }
 }
